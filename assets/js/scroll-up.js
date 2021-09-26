7cd3e8f282f6ed067285f9cec26a900cc2623d07
Jhon Santana <jhalx2198@gmail.com>
2021-09-26 00:01:19 -0500

    Fix fontSize headings mobile

--- assets/js/scroll-up.js
+++ assets/js/scroll-up.js
@@ -1,4 +1,5 @@
 const btnUp = document.querySelector('#btnUp');
+const btnWhatsapp = document.querySelector('#btn-whatsapp');
 const scrollUp = () => {
 	let currentScroll = document.documentElement.scrollTop;
 	if (currentScroll > 0) {
@@ -12,9 +13,10 @@ window.onscroll = () => {
 	let scroll = document.documentElement.scrollTop;
 
 	if (scroll > 100) {
-		console.log('Es mayor a 100');
 		btnUp.style.transform = 'scale(1)';
+		btnWhatsapp.style.transform = 'scale(1)';
 	} else {
 		btnUp.style.transform = 'scale(0)';
+		btnWhatsapp.style.transform = 'scale(0)';
 	}
 };
